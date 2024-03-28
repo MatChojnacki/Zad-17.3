886ea2dd021e8a4be7523cb31549699892fc5b1f
Chojnacki <twojemail@example.com>
2024-03-28 20:38:01 +0100

    Zad 21.2

---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,48 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class TrelloClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+    @Value("${trello.username}")
+    private String trelloUsername;
+
+    private String buildUrl(String path) {
+        return trelloApiEndpoint + path;
+    }
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+            String url = buildUrl("/members/" + trelloUsername + "/boards");
+
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(
+                    url + "?key=" + trelloAppKey + "&token=" + trelloToken + "&fields=name,id",
+                    TrelloBoardDto[].class
+            );
+
+            return Optional.ofNullable(boardsResponse)
+                    .map(Arrays::asList)
+                    .orElse(Collections.emptyList());
+        }
+
+    }
